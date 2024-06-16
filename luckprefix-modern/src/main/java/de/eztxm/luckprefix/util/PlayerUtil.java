c7f3f176142087874dce8ffd43188499612dd43b
ezTxmMC <eztxmmc@hotmail.com>
2024-06-16 14:24:40 +0200

    refactor: added new map for user group

--- luckprefix-modern/src/main/java/de/eztxm/luckprefix/util/PlayerUtil.java
+++ luckprefix-modern/src/main/java/de/eztxm/luckprefix/util/PlayerUtil.java
@@ -1,21 +1,29 @@
 package de.eztxm.luckprefix.util;
 
 import lombok.Getter;
+import net.luckperms.api.model.group.Group;
 import org.bukkit.scheduler.BukkitTask;
 
 import java.util.HashMap;
+import java.util.Map;
 import java.util.UUID;
 
 @Getter
 public class PlayerUtil {
-    private final HashMap<UUID, BukkitTask> joinSchedulers;
+    private final Map<UUID, BukkitTask> joinSchedulers;
+    private final Map<UUID, Group> userGroups;
 
     public PlayerUtil() {
         this.joinSchedulers = new HashMap<>();
+        this.userGroups = new HashMap<>();
     }
 
     public void addJoinScheduler(UUID uuid, BukkitTask bukkitTask) {
         this.joinSchedulers.put(uuid, bukkitTask);
+    }
+
+    public void addUserGroup(UUID uuid, Group group) {
+        this.userGroups.put(uuid, group);
     }
 
     public void cancelJoinScheduler(UUID uuid) {
@@ -24,5 +32,9 @@ public class PlayerUtil {
 
     public void removeJoinScheduler(UUID uuid) {
         this.joinSchedulers.remove(uuid);
+    }
+
+    public void removeUserGroup(UUID uuid) {
+        this.userGroups.remove(uuid);
     }
 }
