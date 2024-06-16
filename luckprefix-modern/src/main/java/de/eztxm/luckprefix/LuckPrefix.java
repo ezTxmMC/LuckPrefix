987f4a2fc62171bf8d0661171934f9e81df4816c
ezTxmMC <eztxmmc@hotmail.com>
2024-06-16 14:27:41 +0200

    feat: moved group create into onEnable

--- luckprefix-modern/src/main/java/de/eztxm/luckprefix/LuckPrefix.java
+++ luckprefix-modern/src/main/java/de/eztxm/luckprefix/LuckPrefix.java
@@ -8,6 +8,7 @@ import de.eztxm.luckprefix.listener.QuitListener;
 import de.eztxm.luckprefix.util.Groups;
 import de.eztxm.luckprefix.util.PlayerUtil;
 import lombok.Getter;
+import net.kyori.adventure.platform.bukkit.BukkitAudiences;
 import net.luckperms.api.LuckPerms;
 import net.luckperms.api.LuckPermsProvider;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -17,6 +18,8 @@ public final class LuckPrefix extends JavaPlugin {
     @Getter
     private static LuckPrefix instance;
 
+    private String prefix;
+    private BukkitAudiences adventure;
     private LuckPerms luckPerms;
     private Registry registry;
     private PlayerUtil playerUtil;
@@ -26,7 +29,16 @@ public final class LuckPrefix extends JavaPlugin {
     @Override
     public void onEnable() {
         instance = this;
+        prefix = "<#77ef77>LuckPrefix <dark_gray>| <gray>";
+        adventure = BukkitAudiences.create(instance);
         luckPerms = LuckPermsProvider.get();
+        luckPerms.getGroupManager().getLoadedGroups().forEach(group -> {
+            if (LuckPrefix.getInstance().getConfig().contains("Groups." + group.getName())) {
+                LuckPrefix.getInstance().getGroups().createGroup(group);
+                return;
+            }
+            LuckPrefix.getInstance().getLogger().warning("§cGroup '" + group.getName() + "' can't be loaded. Please check your config!");
+        });
         registry = new Registry(instance);
         registry.registerCommand("luckprefix", new LuckPrefixCommand());
         registry.registerListener(new JoinListener());
@@ -45,5 +57,7 @@ public final class LuckPrefix extends JavaPlugin {
         instance = null;
         registry = null;
         playerUtil = null;
+        groups = null;
+        groupListener = null;
     }
 }
