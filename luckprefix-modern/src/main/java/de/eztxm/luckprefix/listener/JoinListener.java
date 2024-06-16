947e4d93d2f29f5797c2dbd4d1e06d126836fd6d
ezTxmMC <eztxmmc@hotmail.com>
2024-06-16 14:26:50 +0200

    refactor: recoded join listener

--- luckprefix-modern/src/main/java/de/eztxm/luckprefix/listener/JoinListener.java
+++ luckprefix-modern/src/main/java/de/eztxm/luckprefix/listener/JoinListener.java
@@ -3,7 +3,10 @@ package de.eztxm.luckprefix.listener;
 import de.eztxm.luckprefix.LuckPrefix;
 import net.luckperms.api.LuckPerms;
 import net.luckperms.api.LuckPermsProvider;
+import net.luckperms.api.model.group.Group;
+import net.luckperms.api.model.user.User;
 import org.bukkit.Bukkit;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -15,17 +18,13 @@ public class JoinListener implements Listener {
     @EventHandler
     public void onJoin(PlayerJoinEvent event) {
         Player player = event.getPlayer();
-        LuckPerms luckPermsApi = LuckPermsProvider.get();
-        player.setScoreboard(Bukkit.getScoreboardManager().getNewScoreboard());
-        luckPermsApi.getGroupManager().getLoadedGroups().forEach(group -> {
-            if (LuckPrefix.getInstance().getConfig().contains("Groups." + group.getName())) {
-                LuckPrefix.getInstance().getGroups().createGroup(group);
-                return;
-            }
-            LuckPrefix.getInstance().getLogger().warning("§cGroup '" + group.getName() + "' can't be loaded. Please check your config!");
-        });
-        BukkitTask bukkitTask = Bukkit.getScheduler().runTaskTimerAsynchronously(LuckPrefix.getInstance(),
-                () -> Bukkit.getOnlinePlayers().forEach(players -> LuckPrefix.getInstance().getGroups().setGroup(players)), 5, 20);
+        LuckPerms luckPerms = LuckPermsProvider.get();
+        User user = luckPerms.getUserManager().getUser(player.getUniqueId());
+        Group group = luckPerms.getGroupManager().getGroup(user.getPrimaryGroup());
+        FileConfiguration config = LuckPrefix.getInstance().getConfig();
+        BukkitTask bukkitTask = Bukkit.getScheduler().runTaskTimerAsynchronously(LuckPrefix.getInstance(), () -> {
+            LuckPrefix.getInstance().getGroups().setGroup(group, player);
+        }, 1, config.getLong("UpdateTime"));
         LuckPrefix.getInstance().getPlayerUtil().addJoinScheduler(player.getUniqueId(), bukkitTask);
     }
 }
