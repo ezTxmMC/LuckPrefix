21af0182b0d55eb38966a7f56749ab7afca9e7af
ezTxmMC <eztxmmc@hotmail.com>
2024-06-16 14:26:21 +0200

    feat: added option to set color code char

--- luckprefix-modern/src/main/java/de/eztxm/luckprefix/listener/ChatListener.java
+++ luckprefix-modern/src/main/java/de/eztxm/luckprefix/listener/ChatListener.java
@@ -5,6 +5,7 @@ import net.luckperms.api.LuckPerms;
 import net.luckperms.api.LuckPermsProvider;
 import net.luckperms.api.model.user.User;
 import org.bukkit.ChatColor;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
@@ -15,6 +16,7 @@ public class ChatListener implements Listener {
     public void onChat(AsyncPlayerChatEvent event) {
         LuckPerms luckPerms = LuckPermsProvider.get();
         User user = luckPerms.getUserManager().getUser(event.getPlayer().getUniqueId());
+        FileConfiguration config = LuckPrefix.getInstance().getConfig();
         if (event.getPlayer().hasPermission(LuckPrefix.getInstance().getConfig().getString("ColoredPermission"))) {
             event.setFormat(LuckPrefix.getInstance().getConfig().getString("Groups." + user.getPrimaryGroup() + ".ChatFormat")
                     .replace('&', '§')
@@ -22,7 +24,7 @@ public class ChatListener implements Listener {
                     .replace("%prefix", ChatColor.translateAlternateColorCodes('&',
                             LuckPrefix.getInstance().getConfig().getString("Groups." + user.getPrimaryGroup() + ".Prefix")))
                     .replace("%player", event.getPlayer().getName())
-                    .replace("%message", ChatColor.translateAlternateColorCodes('&', event.getMessage())));
+                    .replace("%message", ChatColor.translateAlternateColorCodes((char) config.get("ColorCodeChar"), event.getMessage().replace("%", "%%"))));
             return;
         }
         event.setFormat(LuckPrefix.getInstance().getConfig().getString("Groups." + user.getPrimaryGroup() + ".ChatFormat")
@@ -31,6 +33,6 @@ public class ChatListener implements Listener {
                 .replace("%prefix", ChatColor.translateAlternateColorCodes('&',
                         LuckPrefix.getInstance().getConfig().getString("Groups." + user.getPrimaryGroup() + ".Prefix")))
                 .replace("%player", event.getPlayer().getName())
-                .replace("%message", event.getMessage()));
+                .replace("%message", event.getMessage().replace("%", "%%")));
     }
 }
