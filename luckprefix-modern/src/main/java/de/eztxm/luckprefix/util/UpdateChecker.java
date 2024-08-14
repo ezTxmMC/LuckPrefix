9127c4b23f4cc153ca0aa2ab2bc178fe1d5db4e0
ezTxmMC <eztxmmc@hotmail.com>
2024-08-14 14:59:39 +0200

    added version cache

--- luckprefix-modern/src/main/java/de/eztxm/luckprefix/util/UpdateChecker.java
+++ luckprefix-modern/src/main/java/de/eztxm/luckprefix/util/UpdateChecker.java
@@ -1,5 +1,6 @@
 package de.eztxm.luckprefix.util;
 
+import lombok.Getter;
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
@@ -7,30 +8,35 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
 
+@Getter
 public class UpdateChecker {
-    private final String version;
+    private final String currentVersion;
+    private String cachedLatestVersion = "N/A";
 
     public UpdateChecker(String version) {
-        this.version = version;
+        this.currentVersion = version;
+        this.cachedLatestVersion = getLatestVersion();
     }
 
     public boolean latestVersion() {
         String latestVersion = getLatestVersion();
         if (latestVersion == null) return true;
-        return latestVersion.equalsIgnoreCase(version);
+        return latestVersion.equalsIgnoreCase(currentVersion);
     }
 
-    public String getLatestVersion() {
+    private String getLatestVersion() {
         try {
             String urlString = "https://cdn.eztxm.de/plugin/luckprefix/manifest.json";
             URL url = new URL(urlString);
             BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream()));
             String line = reader.readLine();
             JSONObject jsonObject = new JSONObject(line);
-            return jsonObject.getString("Latest-Version");
+            String latestVersion = jsonObject.getString("Latest-Version");
+            this.cachedLatestVersion = latestVersion;
+            return latestVersion;
         } catch (IOException e) {
             e.fillInStackTrace();
         }
-        return null;
+        return "N/A";
     }
 }
