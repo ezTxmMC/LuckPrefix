b2ed61fcb68d161f9737985c147974de0efcde5d
ezTxmMC <eztxmmc@hotmail.com>
2024-08-14 15:00:50 +0200

    added option to clear prefix and suffix

--- luckprefix-modern/src/main/java/de/eztxm/luckprefix/command/subcommand/impl/PrefixSubCommand.java
+++ luckprefix-modern/src/main/java/de/eztxm/luckprefix/command/subcommand/impl/PrefixSubCommand.java
@@ -6,11 +6,10 @@ import de.eztxm.luckprefix.util.Text;
 import net.kyori.adventure.audience.Audience;
 import net.luckperms.api.model.group.Group;
 import org.bukkit.configuration.file.FileConfiguration;
-import org.bukkit.entity.Player;
 
 public class PrefixSubCommand {
 
-    public static void execute(Player player, Audience adventurePlayer, Group group, String[] args, FileConfiguration groupsConfig, ConfigManager groupsFile) {
+    public static void execute(Audience adventurePlayer, Group group, String[] args, FileConfiguration groupsConfig, ConfigManager groupsFile) {
         if (args.length > 4) {
             StringBuilder builder = new StringBuilder(args[4]);
             for (int i = 5; i < args.length - 1; i++) {
@@ -18,9 +17,18 @@ public class PrefixSubCommand {
             }
             groupsConfig.set(group.getName().toLowerCase() + ".Prefix", builder.toString());
             groupsFile.reloadConfig();
+            LuckPrefix.getInstance().getGroupManager().reloadGroup(group.getName());
             String prefix = groupsConfig.getString(group.getName().toLowerCase() + ".Prefix");
             adventurePlayer.sendMessage(new Text(LuckPrefix.getInstance().getPrefix()
                     + "The prefix of the group <#33ffff>" + group.getName() + " <gray>is now: " + prefix).miniMessage());
+            return;
+        }
+        if (args.length == 4 && args[3].equalsIgnoreCase("clear")) {
+            groupsConfig.set(group.getName().toLowerCase() + ".Prefix", "");
+            groupsFile.reloadConfig();
+            LuckPrefix.getInstance().getGroupManager().reloadGroup(group.getName());
+            adventurePlayer.sendMessage(new Text(LuckPrefix.getInstance().getPrefix()
+                    + "The prefix of the group <#33ffff>" + group.getName() + " <gray>has been cleared.").miniMessage());
             return;
         }
         String prefix = groupsConfig.getString(group.getName().toLowerCase() + ".Prefix");
--- luckprefix-modern/src/main/java/de/eztxm/luckprefix/command/subcommand/impl/SuffixSubCommand.java
+++ luckprefix-modern/src/main/java/de/eztxm/luckprefix/command/subcommand/impl/SuffixSubCommand.java
@@ -10,7 +10,7 @@ import org.bukkit.entity.Player;
 
 public class SuffixSubCommand {
 
-    public static void execute(Player player, Audience adventurePlayer, Group group, String[] args, FileConfiguration groupsConfig, ConfigManager groupsFile) {
+    public static void execute(Audience adventurePlayer, Group group, String[] args, FileConfiguration groupsConfig, ConfigManager groupsFile) {
         if (args.length > 4) {
             StringBuilder builder = new StringBuilder(args[4]);
             for (int i = 5; i < args.length - 1; i++) {
@@ -18,9 +18,18 @@ public class SuffixSubCommand {
             }
             groupsConfig.set(group.getName().toLowerCase() + ".Suffix", builder.toString());
             groupsFile.reloadConfig();
+            LuckPrefix.getInstance().getGroupManager().reloadGroup(group.getName());
             String suffix = groupsConfig.getString(group.getName().toLowerCase() + ".Suffix");
             adventurePlayer.sendMessage(new Text(LuckPrefix.getInstance().getPrefix()
                     + "The suffix of the group <#33ffff>" + group.getName() + " <gray>is now: " + suffix).miniMessage());
+            return;
+        }
+        if (args.length == 4 && args[3].equalsIgnoreCase("clear")) {
+            groupsConfig.set(group.getName().toLowerCase() + ".Suffix", "");
+            groupsFile.reloadConfig();
+            LuckPrefix.getInstance().getGroupManager().reloadGroup(group.getName());
+            adventurePlayer.sendMessage(new Text(LuckPrefix.getInstance().getPrefix()
+                    + "The suffix of the group <#33ffff>" + group.getName() + " <gray>has been cleared.").miniMessage());
             return;
         }
         String suffix = groupsConfig.getString(group.getName().toLowerCase() + ".Suffix");
