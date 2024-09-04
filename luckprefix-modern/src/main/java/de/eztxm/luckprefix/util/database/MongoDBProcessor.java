d3392d623f2ac4542ed6a8101af5d7aa7089be1f
ezTxmMC <eztxmmc@hotmail.com>
2024-09-04 21:48:45 +0200

    added group methods to mongodb processor

--- luckprefix-modern/src/main/java/de/eztxm/luckprefix/util/database/MongoDBProcessor.java
+++ luckprefix-modern/src/main/java/de/eztxm/luckprefix/util/database/MongoDBProcessor.java
@@ -1,6 +1,8 @@
 package de.eztxm.luckprefix.util.database;
 
+import com.mongodb.client.model.Filters;
 import de.eztxm.database.MongoDBConnection;
+import org.bson.Document;
 
 public class MongoDBProcessor {
     private final MongoDBConnection connection;
@@ -12,5 +14,28 @@ public class MongoDBProcessor {
 
     public void createCollections() {
         this.connection.createCollection("luckprefix_groups");
+    }
+
+    public boolean isGroupExists(String group) {
+        return this.connection.find("luckprefix_groups", Filters.eq("name", group)) != null;
+    }
+
+    public Document getGroup(String group) {
+        return this.connection.find("luckprefix_groups", Filters.eq("name", group));
+    }
+
+    public void updateGroup(String group, Document document) {
+        if (!isGroupExists(group)) return;
+        this.connection.update("luckprefix_groups", Filters.eq("name", group), document);
+    }
+
+    public void addGroup(String group, Document document) {
+        if (isGroupExists(group)) return;
+        this.connection.insert("luckprefix_groups", document);
+    }
+
+    public void removeGroup(String group) {
+        if (!isGroupExists(group)) return;
+        this.connection.delete("luckprefix_groups", Filters.eq("name", group));
     }
 }
