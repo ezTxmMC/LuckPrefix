9d9f9a4c9ba1e85e1193b7cf5ed17de7569e7424
ezTxmMC <eztxmmc@hotmail.com>
2024-12-24 11:44:22 +0100

    enabled development and changed version to 2.0.0-BETA+8-SNAPSHOT

--- luckprefix-modern/src/main/java/de/eztxm/luckprefix/LuckPrefix.java
+++ luckprefix-modern/src/main/java/de/eztxm/luckprefix/LuckPrefix.java
@@ -20,7 +20,7 @@ public final class LuckPrefix extends JavaPlugin {
     @Getter
     private static LuckPrefix instance;
     @Getter
-    private static boolean development = false;
+    private static boolean development = true;
 
     private String prefix;
     private ConfigManager databaseFile;
