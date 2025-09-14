c0d2f1ffaba6aa3f639189180b57f481d160e5d5
Tom <eztxmmc@hotmail.com>
2025-09-14 22:14:22 +0200

    fixed possible zip error

--- luckprefix-modern-spigot/src/main/java/de/eztxm/luckprefix/listener/GroupListener.java
+++ luckprefix-modern-spigot/src/main/java/de/eztxm/luckprefix/listener/GroupListener.java
@@ -9,16 +9,7 @@ import net.luckperms.api.event.group.GroupDataRecalculateEvent;
 import net.luckperms.api.event.group.GroupDeleteEvent;
 import net.luckperms.api.event.user.UserDataRecalculateEvent;
 
-public class GroupListener {
-    private final LuckPerms luckPerms;
-    private final GroupManager groupManager;
-    private final PlayerManager playerManager;
-
-    public GroupListener(LuckPerms luckPerms, GroupManager groupManager, PlayerManager playerManager) {
-        this.luckPerms = luckPerms;
-        this.groupManager = groupManager;
-        this.playerManager = playerManager;
-    }
+public record GroupListener(LuckPerms luckPerms, GroupManager groupManager, PlayerManager playerManager) {
 
     public void onCreateGroup() {
         EventBus eventBus = this.luckPerms.getEventBus();
@@ -29,7 +20,7 @@ public class GroupListener {
         EventBus eventBus = this.luckPerms.getEventBus();
         eventBus.subscribe(GroupDeleteEvent.class, event -> this.groupManager.deleteGroup(event.getGroupName()));
     }
-    
+
     public void onUpdateGroup() {
         EventBus eventBus = this.luckPerms.getEventBus();
         eventBus.subscribe(GroupDataRecalculateEvent.class, event -> {
@@ -40,7 +31,6 @@ public class GroupListener {
 
     public void onUpdateUserGroup() {
         EventBus eventBus = this.luckPerms.getEventBus();
-        eventBus.subscribe(UserDataRecalculateEvent.class, event ->
-                this.playerManager.setUserGroup(event.getUser().getUniqueId(), event.getUser().getPrimaryGroup()));
+        eventBus.subscribe(UserDataRecalculateEvent.class, event -> this.playerManager.setUserGroup(event.getUser().getUniqueId(), event.getUser().getPrimaryGroup()));
     }
 }
